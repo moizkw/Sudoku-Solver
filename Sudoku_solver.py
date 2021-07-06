829c1e377b8fe025bc7302c680f998484fc0d0c8
moizkw <86135505+moizkw@users.noreply.github.com>
2021-07-06 18:02:46 -0400

    Update Sudoku_solver.py

--- Sudoku_solver.py
+++ Sudoku_solver.py
@@ -40,7 +40,7 @@ def sudoku_solve(sudoku):
 
     row, col = find_empty_box(sudoku) #choosing any empty box to make a guess  
 
-    if row is None: #this is true if our find_empty_box function returns None, None
+    if row is None: #this is true if the find_empty_box function returns None, None
         return True
 
     for guess in range(1, 10): #guess can be any number from 1 to 9
@@ -50,9 +50,9 @@ def sudoku_solve(sudoku):
             if sudoku_solve(sudoku):
                 return True
 
-        sudoku[row][col] = -1 #if not valid or if nothing gets returned true, then we need to backtrack and try a new number
+        sudoku[row][col] = -1 #if not valid or if nothing gets returned true, then it needs to backtrack and try a new number
     
-    return False #if none of the numbers that we try work, then this puzzle is UNSOLVABLE!!
+    return False #if none of the numbers work, then this puzzle is UNSOLVABLE!!
 
 
 if __name__ == '__main__':
